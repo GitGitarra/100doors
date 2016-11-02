40fea29dd31bb6785099df99f4ba04764fce56cc
GitGitarra <monika.plocica@gmail.com>
2016-11-02 22:49:46 +0100

    Update 100doors.py

--- 100doors.py
+++ 100doors.py
@@ -1,21 +1,18 @@
-doors = [0]*100
+doors = [False]*100    #create 100 doors, where 'False' means a closed door 
 
-for x in range(1, 100):
+for x in range(100):    #number of passes by the hallway
 
-    for y in range(x, 100, x+1):
+    for y in range(x, 100, x+1):    #with each pass, the number of steps between the door and increased by one 
 
-        doors[y] = not doors[y]
+        doors[y] = not doors[y]     #change the status of the door to the opposite
 
-print('The following doors are open:', end=' ')
+print('The following doors are open: ', end='')    #end='' to continue in the same line
 
-for i, value in enumerate(doors, start=1):
+for i, value in enumerate(doors, start=1): #return index and value of doors, start counting from 1
 
-        if value == 1:
+        if value is True:     #checking value of the doors
             if i == len(doors):
                 print(i)
-        else:
-            if i < 100:
-                print(i, end=', ')
             else:
-                print(i, end='.')
+                print(i, end=', ')    #prints the results after comma
 exit()
